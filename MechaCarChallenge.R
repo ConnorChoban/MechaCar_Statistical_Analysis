d39fc9c47cb8397ac4821dcfc9150c2c17c7ab47
ConnorChoban <99847786+ConnorChoban@users.noreply.github.com>
2022-06-11 16:51:56 -0400

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+mechaCar <- read.csv("MechaCar_mpg.csv", check.names=F, stringsAsFactors = F)
+head(mechaCar)
+
+#For the deliverable we'll compare MPG against all factors/columns but any other variable could be used as well
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar))
+
